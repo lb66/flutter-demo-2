c90477a265b944e9da20b12f2939e1ad6ecc11e8
lb66 <807242867@qq.com>
2020-11-17 17:25:59 +0800

    Container容器组件

--- lib/main.dart
+++ lib/main.dart
@@ -12,17 +12,36 @@ class MyApp extends StatelessWidget {
         home: Scaffold(
           appBar: AppBar(title: Text('welcome')),
           body: Center(
-            child: Text(
-              'hello,world,hello,world,hello,world,hello,world,hello,world',
-              textAlign: TextAlign.left,
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-              style: TextStyle(
-                fontSize: 25.0,
-                color: Color.fromARGB(255, 255, 150, 150),
-                decoration: TextDecoration.underline,
-                decorationStyle: TextDecorationStyle.solid,
+            // child: Text(
+            //   'hello,world,hello,world,hello,world,hello,world,hello,world',
+            //   textAlign: TextAlign.left,
+            //   maxLines: 1,
+            //   overflow: TextOverflow.ellipsis,
+            //   style: TextStyle(
+            //     fontSize: 25.0,
+            //     color: Color.fromARGB(255, 255, 150, 150),
+            //     decoration: TextDecoration.underline,
+            //     decorationStyle: TextDecorationStyle.solid,
+            //   ),
+            // ),
+            child: Container(
+              child: new Text(
+                'Hello World',
+                style: TextStyle(fontSize: 40.0),
               ),
+              alignment: Alignment.topLeft,
+              width: 500.0,
+              height: 400.0,
+              // color: Colors.lightBlue,
+              padding: const EdgeInsets.fromLTRB(10.0, 30.0, 0.0, 0.0),
+              margin: const EdgeInsets.all(10.0),
+              decoration: new BoxDecoration(
+                  gradient: const LinearGradient(colors: [
+                    Colors.lightBlue,
+                    Colors.greenAccent,
+                    Colors.purple
+                  ]),
+                  border: Border.all(width: 2.0, color: Colors.red)),
             ),
           ),
         ));
